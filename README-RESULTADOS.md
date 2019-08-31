af77cfaeb1db8fcd3df5a75943a557c282be1ecf
Maria Lourdes Garcia Florez <marylo281292@gmail.com>
2019-08-31 00:42:08 -0400

    desafio nivel 2 y agregando readme

---
+++ README-RESULTADOS.md
@@ -0,0 +1,22 @@
+# Respuesta al desafio uno
+
+## Información
+
+Se realizó el desafío nivel 2, usando python3
+
+## Para Correr
+
+* Ejecutar el generador de datos disponible en el repositorio en GitHub:
+https://github.com/previred/Generador_Datos_Desafio_Uno
+
+* ejecutar `python3 desafio.py`
+
+Se creará un archivo *input.json* con la entrada y otro llamado *output.json* con el resultado
+
+## Resultados Ejecución
+
+Los archivos entregados son los siguientes:
+
+* *desafio.py* con el código implementado
+* *input.json* con la entrada de una corrida
+* *output.json* con la salida de la corrida
--- desafio.py
+++ desafio.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import requests 
 
 def getMissingDates(startDate, endDate, arrayDates):
     startDateArray = startDate.split('-')
@@ -10,7 +11,7 @@ def getMissingDates(startDate, endDate, arrayDates):
     
     for year in range(startDateArray[0], endDateArry[0]+1):
         for month in range(1, 13):             
-            if not ((year == startDateArray[0] and month < startDateArray[1]) or (year == endDateArry[0] and month >  endDateArry[1])):
+            if not ((year == startDateArray[0] and month < startDateArray[1]) or (year == endDateArry[0] and month > endDateArry[1])):
                 dateAux = str(year)+'-'+str(month).zfill(2)+'-01'
                 if dateAux not in arrayDates:
                     missingDates.append(dateAux)        
@@ -23,15 +24,20 @@ def createResult(data):
         "id": data['id'],
         "fechaCreacion": data['fechaCreacion'],
         "fechaFin": data['fechaFin'],
+        "fechas recibidas": data['fechas'],
         "fechasFaltantes": missingDates
     }
     
     return result
 
-with open(sys.argv[1], 'r') as inputFile:
-    data = json.load(inputFile)
-    
-with open(sys.argv[2], 'w') as outputFile:
-        json.dump(createResult(data), outputFile, indent=4)
+url = 'http://127.0.0.1:8080/periodos/api'
+header = {'Accept': 'application/json'}
 
-print(createResult(data))
+dataJson = requests.get( url=url, headers=header)
+data = dataJson.json()
+
+with open('input.json', 'w') as inputFile:
+    json.dump(data, inputFile, indent=4)
+    
+with open('output.json', 'w') as outputFile:
+    json.dump(createResult(data), outputFile, indent=4)
