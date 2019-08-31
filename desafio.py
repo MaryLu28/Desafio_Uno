f4fcd25dd519d71bf65604062503ab8e9c773de0
Maria Lourdes Garcia Florez <marylo281292@gmail.com>
2019-08-30 23:44:15 -0400

    desafio nivel 1 completado

--- desafio.py
+++ desafio.py
@@ -1,22 +1,37 @@
 import json
+import sys
 
 def getMissingDates(startDate, endDate, arrayDates):
     startDateArray = startDate.split('-')
     startDateArray = list(map(int, startDateArray))
     endDateArry = endDate.split('-')
     endDateArry = list(map(int, endDateArry))
-    datesMissing = []
+    missingDates = []
     
     for year in range(startDateArray[0], endDateArry[0]+1):
         for month in range(1, 13):             
             if not ((year == startDateArray[0] and month < startDateArray[1]) or (year == endDateArry[0] and month >  endDateArry[1])):
                 dateAux = str(year)+'-'+str(month).zfill(2)+'-01'
                 if dateAux not in arrayDates:
-                    datesMissing.append(dateAux)        
-                  
-    return(datesMissing)
+                    missingDates.append(dateAux)        
+               
+    return(missingDates)
 
-with open('test.json') as dataFile:
-    data = json.load(dataFile)
+def createResult(data):
+    missingDates = getMissingDates(data['fechaCreacion'],data['fechaFin'],data['fechas'])
+    result = {
+        "id": data['id'],
+        "fechaCreacion": data['fechaCreacion'],
+        "fechaFin": data['fechaFin'],
+        "fechasFaltantes": missingDates
+    }
+    
+    return result
 
-print(getMissingDates(data['fechaCreacion'],data['fechaFin'],data['fechas']))
+with open(sys.argv[1], 'r') as inputFile:
+    data = json.load(inputFile)
+    
+with open(sys.argv[2], 'w') as outputFile:
+        json.dump(createResult(data), outputFile, indent=4)
+
+print(createResult(data))
